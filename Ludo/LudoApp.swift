0611dc9e617f4f779c7e5ee21f794142719f2a41
Shiva garg <shivagarg@Shivas-MacBook-Air.local>
2025-08-16 23:47:58 +0100

    added mirchi labs splash  screen

--- Ludo/LudoApp.swift
+++ Ludo/LudoApp.swift
@@ -2,9 +2,27 @@ import SwiftUI
 
 @main
 struct LudoApp: App {
+    @State private var isShowingSplash = true
+
     var body: some Scene {
         WindowGroup {
-            LudoGameView()
+            if isShowingSplash {
+                SplashScreenView()
+                    .onAppear {
+                        #if DEBUG
+                        let delay = 5.0
+                        #else
+                        let delay = 3.0
+                        #endif
+                        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+                            withAnimation {
+                                self.isShowingSplash = false
+                            }
+                        }
+                    }
+            } else {
+                LudoGameView()
+            }
         }
     }
 } 
---
+++ Ludo/SplashScreenView.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+
+struct SplashScreenView: View {
+    @State private var isBouncing = false
+
+    var body: some View {
+        GeometryReader { geometry in
+            let fontSize = geometry.size.width * 0.1
+            ZStack {
+                Color.black.edgesIgnoringSafeArea(.all)
+                HStack(alignment: .firstTextBaseline, spacing: 0) {
+                    Text("m")
+                    Image("pawn_mirchi")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(height: fontSize)
+                        .padding(.horizontal, -fontSize * 0.15)
+                        .offset(y: fontSize * 0.15 + (isBouncing ? -fontSize * 0.2 : 0))
+                    Text("rchi labs")
+                }
+                .font(.custom("Chalkboard SE", size: fontSize))
+                .foregroundColor(.red)
+            }
+        }
+        .onAppear {
+            withAnimation(.easeInOut(duration: 0.6).repeatForever(autoreverses: true)) {
+                isBouncing = true
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
